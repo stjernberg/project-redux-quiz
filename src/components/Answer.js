4d0266e07b2026d4d105482f1d416eff3a8cd257
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2020-11-17 16:57:55 +0100

    answer onClick not working correctly

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -8,28 +8,35 @@ import styled from 'styled-components';
 export const Answer = ({ answer, index, questionId }) => {
   const dispatch = useDispatch();
 
-
   const answerValue = useSelector(
     (state) => state.quiz.answers
   );
-console.log(answerValue)
+
+  const onNext = () => {
+    dispatch(
+      quiz.actions.submitAnswer({
+        questionId: questionId,
+        answerIndex: index,
+      }));
+    if (answerValue.length > 0) {
+      console.log('I EXIST')
+      console.log(answerValue)
+    } else {
+      console.log('NO')
+    }
+    checkAnswer();
+  }
   const checkAnswer = (index) => {
     console.log(answerValue)
-    if (answerValue[questionId - 1].isCorrect) {
-       console.log ('correct')
-    }
+    
+    // if (answerValue[questionId - 1].isCorrect) {
+    //    console.log ('correct')
+    // }
   }
   return (
     <div>
       <AnswerButton 
-        onClick={() => 
-          dispatch(
-            quiz.actions.submitAnswer({
-              questionId: questionId,
-              answerIndex: index,
-            })), () => checkAnswer()
-          
-        }
+        onClick={() => onNext()}
       >
         {answer}
       </AnswerButton>
